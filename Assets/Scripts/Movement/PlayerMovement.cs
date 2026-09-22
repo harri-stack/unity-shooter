69008d42c8d6ca1359e433921bf663f0fdd394cd
Harrison <harribibbles@gmail.com>
2026-09-22 18:43:41 +0100

    Add jump and gravity physics

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -7,12 +8,27 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
     public float Speed = 3.5f;
+    public float Gravity = 9.5f;
+    public float jumpHeight = 2f;
+    private float yVelocity;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
     private void Update()
     {
+        if (Keyboard.current.spaceKey.isPressed && characterController.isGrounded)
+        {
+            yVelocity = Mathf.Sqrt(jumpHeight * 2f * Gravity);
+        }
+
+
+        if (characterController.isGrounded && yVelocity < 0)
+        {
+            yVelocity = -2f;
+        }
+
+        yVelocity -= Gravity * Time.deltaTime;
         Vector3 Direction = Vector3.zero;
 
         if (Keyboard.current.wKey.isPressed)
@@ -31,6 +47,7 @@ public class PlayerMovement : MonoBehaviour
         {
             Direction -= transform.right;
         }
+
         if (Keyboard.current.leftShiftKey.isPressed)
         {
             Speed = 7.0f;
@@ -42,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
 
         Direction = Direction.normalized;
         Vector3 finalDirection = Direction * Speed;
+        finalDirection.y = yVelocity;
         characterController.Move(finalDirection * Time.deltaTime);
     }
 }
--- DEV-LOG.md
+++ DEV-LOG.md
@@ -1,12 +1,30 @@
 # Development log of shooter microgame
 
 ## 22/09/26
-    
+
 ### Problem
-Player movement wasn't working correctly, this is because speed would stack on the update loop. 
+
+Player movement wasn't working correctly, this is because speed would stack on the update loop.
 
 ### Solution
+
 Fix was moving the speed reset line inside of the loop
 
+
+
+\## Another problem
+
+The jump physics were weird and destroyed
+
+
+
+\## Solution
+
+I researched the equation for vertical motion and integrated that into my script 
+
+
+
 ### Additions to the game
-Player movement + FPS camera controls added
+
+Player movement + FPS camera controls and gravity added
+
